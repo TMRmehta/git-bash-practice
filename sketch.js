405c63e38a6ac83c8812ee9d2c8eae6c471f7391
Tushar Mehta <tusharmehta@Tushars-MacBook-Pro.local>
2020-08-11 13:31:59 -0400

    new box created

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var box7
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,6 +34,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    box7 = new Box(400,150,50,50);
 
     bird = new Bird(100,100);
 
@@ -58,6 +60,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    box7.display();
 
     bird.display();
     platform.display();
